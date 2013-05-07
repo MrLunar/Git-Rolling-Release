37d6e9f38eec7b6a68a4e6c0588efbf335da65a4
Mark Bowker <bowker.mark@gmail.com>
2013-05-07 10:21:47 +0200

    Updated releases.php

--- releases.php
+++ releases.php
@@ -2,26 +2,10 @@
 
 /**
  * List of releases for Git-Rolling-Release to manage.
+ * 
+ * @see releases-example.php
  */
 $releases = array
 (
-  'live' => array(
-    'name'      => 'release-live',
-    'tag_from'  => 'tags/release-staging-current',
-    'auto'      => true,
-    'days'      => array('mon'),
-  ),
-
-  'staging' => array(
-    'name'      => 'release-staging',
-    'tag_from'  => 'master',
-    'auto'      => true,
-    'days'      => array('mon', 'tue', 'wed', 'thu'),
-  ),
-
-  'mitre' => array(
-    'name'      => 'release-mitre',
-    'tag_from'  => 'live',
-    'auto'      => false,
-  ),
+  
 );
